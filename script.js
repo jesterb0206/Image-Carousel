c13fe5adc2b06c8ba7dd1256c8206fe008d97ea8
jesterb0206 <jesterb@seattleu.edu>
2023-05-03 23:36:01 -0700

    Fixed Broken Carousel Animation

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const imgs = document.getElementById('imgs');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+
 const img = document.querySelectorAll('#imgs img');
 
 let idx = 0;
+
 let interval = setInterval(run, 2000);
 
 function run() {
@@ -17,6 +19,7 @@ function changeImage() {
   } else if (idx < 0) {
     idx = img.length - 1;
   }
+
   imgs.style.transform = `translateX(${-idx * 500}px)`;
 }
 
